0a0c5a6f395b2c5630909f93f0e8b9d66e284cd4
Hamad <hamad.sa.ghosen@gmail.com>
2024-08-24 22:40:13 +0300

    Add Endpoin_API Viewset For Register users With Return Token Auto

--- api/urls.py
+++ api/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
 from rest_framework.routers import DefaultRouter
 from django.conf.urls import include
-from .views import MealViewSet, RatingViewSet
+from .views import MealViewSet, RatingViewSet , UserViewset
 
 router = DefaultRouter()
 router.register('meals', MealViewSet)
 router.register('ratings', RatingViewSet)
+router.register('users', UserViewset)
 
 urlpatterns = [
-    path('', include(router.urls)),
+    path('' , include(router.urls)),
 ]
--- api/views.py
+++ api/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.contrib.auth.models import User
 from .models import Meal, Rating
-from .serializers import MealSerializer, RatingSerializer
+from .serializers import MealSerializer, RatingSerializer , UserSerializer
 
 from rest_framework import viewsets
 from rest_framework.decorators import action
@@ -9,6 +9,7 @@ from rest_framework.response import Response
 from rest_framework import status , filters
 from rest_framework.authentication import TokenAuthentication,BasicAuthentication
 from rest_framework.permissions import IsAuthenticated,AllowAny,IsAdminUser,IsAuthenticatedOrReadOnly
+from rest_framework.authtoken.models import Token
 # Create your views here.
 
 
@@ -88,3 +89,24 @@ class RatingViewSet(viewsets.ModelViewSet):
             "message":"Invalid way to URL Create rating"
         }
         return Response(response,status=status.HTTP_400_BAD_REQUEST)
+    
+class UserViewset(viewsets.ModelViewSet):
+    queryset=User.objects.all()
+    serializer_class=UserSerializer
+    
+    authentication_classes=[TokenAuthentication]
+    permission_classes=[IsAuthenticated]
+    
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        token, created = Token.objects.get_or_create(user=serializer.instance)
+        return Response({
+                'token': token.key, 
+                }, 
+            status=status.HTTP_201_CREATED)
+        
+    def list(self, request, *args, **kwargs):
+        response = {'message': 'You cant create rating like that'}
+        return Response(response, status=status.HTTP_400_BAD_REQUEST)
